095fd67427df47be942f09c95f210ae1bd1e4605
Lavanya Joshi <lavanyajoshi889@gmail.com>
2026-09-16 18:28:58 +0530

    Fix producer-consumer synchronization issues

--- producer_consumer.c
+++ producer_consumer.c
@@ -7,24 +7,24 @@
 #define BUFFER_SIZE=5
 #define ITEMS=10
 
-int in=0;
-int out=0;
+int in=0; //index where producer produces next item
+int out=0; //index where consumer consumes next item
 int buffer[BUFFER_SIZE];
-sem_t empty=0;
-semt_t full=0;
-pthread_mutex_t mutex;
+sem_t empty=0; //counts empty slots
+semt_t full=0; //counts full slots
+pthread_mutex_t mutex; //ensures mutual exclusion
 
 void *producer(void *arg){
 int item;
 for(int i=0;i<ITEMS;i++){
-item=rand()%100;
-sem_wait(&empty);
-pthread_mutex_lock(&mutex);
-buffer[in]=item;
+item=rand()%100; //produces random item
+sem_wait(&empty); //waits if the buffer is empty
+pthread_mutex_lock(&mutex); //enters ans locks 
+buffer[in]=item; //add to the buffer
 printf("producer produce %d at index %d",item,in);
-in=(in+1)%BUFFER_SIZE;
-pthread_mutex_unlock(&mutex);
-sem_post(&full);
+in=(in+1)%BUFFER_SIZE; //moves index circularly
+pthread_mutex_unlock(&mutex); //unlocks
+sem_post(&full); //signal item available
 unsleep(10000);
 }
 return NULL;
@@ -34,13 +34,13 @@ return NULL;
 void *consumer(voiud *arg){
 int item;
 for(int i=0;i<ITEMS;i++){
-sem_wait(&&full);
-pthread_mutex_lock(&mutex);
-item=buffer[out];
+sem_wait(&&full); //waits if the buffer is empty
+pthread_mutex_lock(&mutex); // goes inside and locks
+item=buffer[out]; //removes from buffer
 printf("consumer consumed %d at index %d",item,out);
 out=(out+1)%BUFFER_SIZE;
-pthread_mutex_unlock(&mutex);
-sem_post(&empty);
+pthread_mutex_unlock(&mutex); //unlocks
+sem_post(&empty); ///signals empty
 unsleep(15000);
 }
 return NULL;
